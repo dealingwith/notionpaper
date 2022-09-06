92daa7f8db9c8a3a315eb5b9509aa38242b9af98
Daniel Miller <dealingwith@gmail.com>
2022-09-06 07:40:59 -0500

    formatting and output tweaks

--- notionpaper.rb
+++ notionpaper.rb
@@ -1,53 +1,56 @@
-require "notion_ruby"
-load "config.rb"
+require 'notion_ruby'
+load 'config.rb'
 # Set these variables in config.rb:
 # NOTION_API_KEY
 # NOTION_BASE_URL
 # NOTION_DB_ID
 
-notion = NotionRuby.new({ access_token: NOTION_API_KEY }) 
+notion = NotionRuby.new({ access_token: NOTION_API_KEY })
 query = {
   "filter": {
     "and": [
       {
-        "property": "Status",
+        "property": 'Status',
         "select": {
-          "does_not_equal": "Done"
+          "does_not_equal": 'Done'
         }
       },
       {
-        "property": "Status",
+        "property": 'Status',
         "select": {
-          "does_not_equal": "Archive"
+          "does_not_equal": 'Archive'
         }
       }
     ]
   },
   "sorts": [
     {
-        "property": "Status",
-        "direction": "descending"
+      "property": 'Status',
+      "direction": 'descending'
     },
     {
-        "property": "Created",
-        "direction": "descending"
+      "property": 'Created',
+      "direction": 'descending'
     }
   ],
   page_size: 100
 }
-query = notion.databases(NOTION_DB_ID).query(query)
+results = notion.databases(NOTION_DB_ID).query(query)
 
-tasks = query['results']
+tasks = results['results']
+
+File.write 'tasks.rb', tasks.to_s
 
 taskpaper_content = ''
 markdown_content = ''
 
 tasks.each do |task|
-  title = task['properties']['Name']['title'][0]['plain_text']
+  title = task['properties']['Name']['title'][0]['plain_text'].strip
+  url = "#{NOTION_BASE_URL}#{title.tr(" ", "-")}-#{task['id'].tr("-", "")}"
   taskpaper_content << "- #{title}\n"
-  taskpaper_content << "  #{NOTION_BASE_URL}#{task['id']}\n"
-  markdown_content << "- [ ] [#{title}](#{NOTION_BASE_URL}#{task['id']})\n"
+  taskpaper_content << "  #{url}\n"
+  markdown_content << "- [ ] [#{title}](#{url})\n"
 end
 
-File.write "notion.taskpaper", taskpaper_content
-File.write "notion.markdown", markdown_content
+File.write 'notion.taskpaper', taskpaper_content
+File.write 'notion.markdown', markdown_content
