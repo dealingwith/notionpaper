bc63d21be48b2978e6ba9f1ef8e710da81cf8043
Daniel Miller <dealingwith@gmail.com>
2022-12-18 17:50:38 -0600

    fix markdown output

--- run.rb
+++ run.rb
@@ -61,9 +61,8 @@ end
 
 tasks = get_notion_tasks(config)
 
-date_string = "Data fetched on #{Date.today.strftime('%Y-%m-%d')}\n\n"
-taskpaper_content = date_string
-markdown_content = date_string
+taskpaper_content = "Data fetched on #{Date.today.strftime('%Y-%m-%d')}\n\n"
+markdown_content = "Data fetched on #{Date.today.strftime('%Y-%m-%d')}\n\n"
 
 tasks.each do |task|
   title = task.dig('properties', 'Name', 'title', 0, 'plain_text')
